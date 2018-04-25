01e5f2d9c3e9fc7248a872dc343cbcca1bfab2ad
Og B. Maciel <omaciel@ogmaciel.com>
2018-04-25 18:37:22 -0400

    Added 2 new methods to handle Plans.

--- polarizer/plan.py
+++ polarizer/plan.py
@@ -1,6 +1,32 @@
 from polarizer.clients import PlanClient
 
 
+def create_plan(
+    config,
+    project_id,
+    plan_name,
+    plan_id=None,
+    parent_id=None,
+    template_id='release',
+):
+    '''Create a new Polarion Plan.'''
+    client = PlanClient(config)
+    return client.service.createPlan(
+        project_id,
+        plan_name,
+        plan_id,
+        parent_id,
+        template_id,
+        )
+
+
+def delete_plan(config, productId='RHSAT6', planId='Satellite_6_4_0'):
+    '''Delete an existing Polarion plan by its ID.'''
+    client = PlanClient(config)
+    return client.service.deletePlans(productId, [planId])
+
+
 def get_plan(config, productId='RHSAT6', planId='Satellite_6_4_0'):
+    '''Fetch an existing Polarion Plan by its ID.'''
     client = PlanClient(config)
     return client.service.getPlanById(productId, planId)
